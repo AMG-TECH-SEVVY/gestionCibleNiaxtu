365ad1336234407a7c313e13e260a23d7b60f5b1
CodeNinja <14mbaye28@gmail.com>
2024-09-18 20:07:27 +0000

    ajout @Repository

--- src/main/java/com/Niaxtu/gestionCible/repository/CibleRepository.java
+++ src/main/java/com/Niaxtu/gestionCible/repository/CibleRepository.java
@@ -2,6 +2,8 @@ package com.Niaxtu.gestionCible.repository;
 
 import com.Niaxtu.gestionCible.entities.CiblePrivee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CibleRepository extends JpaRepository<CiblePrivee,Long> {
 }
--- src/main/java/com/Niaxtu/gestionCible/repository/StructureRepository.java
+++ src/main/java/com/Niaxtu/gestionCible/repository/StructureRepository.java
@@ -2,7 +2,9 @@ package com.Niaxtu.gestionCible.repository;
 
 import com.Niaxtu.gestionCible.entities.Structure;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StructureRepository extends JpaRepository<Structure, Long> {
     // Méthodes de requêtes spécifiques peuvent être ajoutées ici
 }
